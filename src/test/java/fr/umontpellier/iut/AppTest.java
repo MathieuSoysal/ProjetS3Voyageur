63b158795cf0a56ba0091a2bb08fec6e76e06d80
Matthias DELON <matthias.delon@etu.umontpellier.fr>
2019-09-12 16:07:41 +0200

    Mise en place de la structure de dévellopement.

--- src/test/java/fr/umontpellier/iut/AppTest.java
+++ src/test/java/fr/umontpellier/iut/AppTest.java
@@ -1,32 +1,12 @@
 package fr.umontpellier.iut;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.runner.RunWith;
+import org.junit.Before;
 
 import static org.junit.Assert.*;
 
-@RunWith(Arquillian.class)
 public class AppTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(App.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
 
-    @org.junit.Before
+    @Before
     public void setUp() throws Exception {
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-    }
-
-    @org.junit.Test
-    public void main() {
     }
 }
