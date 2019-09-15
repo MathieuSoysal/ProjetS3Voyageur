b89378c1cc2e0926a828fe40f313e8d24e88f5e0
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-09-15 13:19:51 +0200

    Initialisation Projet Maven

---
+++ voyageur/src/main/java/voyageur/App.java
@@ -0,0 +1,13 @@
+package voyageur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
