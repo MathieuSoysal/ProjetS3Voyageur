00852ccb57967523ce9930a1fe2ee79829c2674a
RAVET-LECOURT Florian <florian.ravet-lecourt@etu.umontpellier.fr>
2019-09-19 14:07:59 +0200

    C'est trop bien

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -3,6 +3,6 @@ package projetS3Voyageur;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        System.out.println("Hello World ! c'est trop bien");
     }
 }
