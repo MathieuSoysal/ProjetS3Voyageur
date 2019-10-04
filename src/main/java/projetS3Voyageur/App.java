87f68cf5002b0c83a46e3db144b7d89c09081ed9
Matthias-DELON <matthias.delon@etu.umontpellier.fr>
2019-10-04 14:21:04 +0200

    Suppression classes inutile

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -6,25 +6,15 @@ public class App {
 
     public static void main(String[] args) {
 
-        Timer timer = new Timer(true);
-        InterruptTimerTask interruptTimerTask = new InterruptTimerTask(Thread.currentThread());
-        timer.schedule(interruptTimerTask, 10000);
+        Graphe g = new Graphe(100, 11);
+        System.err.println(g);
 
-        try {
-            Graphe g = new Graphe(100, 5);
-            System.err.println(g);
+        Resolveur r = new Resolveur(g, g.getOneSommet(0).getVille());
 
-            Resolveur r = new Resolveur(g, g.getOneSommet(0).getVille());
+        Trajet t = r.bruteForce();
 
-            Trajet t = r.bruteForce();
-
-            System.out.println("Trajet: " + t);
-            System.out.println("Distance: " + t.distance());
-        }catch (InterruptedException e) {
-            System.out.println("timeout exeeded");
-        }finally {
-            timer.cancel();
-        }
+        System.out.println("Trajet: " + t);
+        System.out.println("Distance: " + t.distance());
 
     }
 }
