0c93a779861211acf0f6e694a93062809ed7fd13
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-10-05 15:25:11 +0200

    Ajout Test: Distance linaire en Y puis X

--- src/test/java/projetS3Voyageur/TestBrutForce.java
+++ src/test/java/projetS3Voyageur/TestBrutForce.java
@@ -8,18 +8,38 @@ import org.junit.Test;
 public class TestBrutForce {
     private BrutForce brutForce;
 
+    @Test
+    public void test_si_les_villes_ont_des_positions_random() {
+
+        brutForce = new BrutForce(new Pays(6)); // initialisation
+
+        brutForce.recherche();
+
+        String parcours1 = brutForce.getParcour().getVillesEmprunté();
+
+        brutForce = new BrutForce(new Pays(6)); // réinitialisation
+
+        brutForce.recherche();
+
+        assertNotEquals(parcours1, brutForce.getParcour().getVillesEmprunté());
+
+    }
+
+    // #region distance linaire sur Y
 
     @Test
-    public void test_ParcourOptimum_Pour_4villes() {
+    public void test_distanceLinaireSurY_ParcourOptimum_Pour_4villes() {
 
         Pays pays = new Pays(4);
 
+        int positionX = (int) Math.random() * 50;
+
         brutForce = new BrutForce(pays);
 
-        pays.setPositionVille(0, new Position(2, 2));
-        pays.setPositionVille(1, new Position(2, 3));
-        pays.setPositionVille(2, new Position(2, 4));
-        pays.setPositionVille(3, new Position(2, 5));
+        pays.setPositionVille(0, new Position(positionX, 2));
+        pays.setPositionVille(1, new Position(positionX, 3));
+        pays.setPositionVille(2, new Position(positionX, 4));
+        pays.setPositionVille(3, new Position(positionX, 5));
 
         brutForce.recherche();
 
@@ -28,18 +48,19 @@ public class TestBrutForce {
     }
 
     @Test
-    public void test_ParcourOptimum_Pour_5villes() {
+    public void test_distanceLinaireSurY_ParcourOptimum_Pour_5villes() {
 
         Pays pays = new Pays(5);
 
         brutForce = new BrutForce(pays);
 
-        pays.setPositionVille(0, new Position(2, 2));
-        pays.setPositionVille(1, new Position(2, 3));
-        pays.setPositionVille(2, new Position(2, 4));
-        pays.setPositionVille(3, new Position(2, 5));
-        pays.setPositionVille(4, new Position(2, 6));
+        int positionX = (int) Math.random() * 50;
 
+        pays.setPositionVille(0, new Position(positionX, 2));
+        pays.setPositionVille(1, new Position(positionX, 3));
+        pays.setPositionVille(2, new Position(positionX, 4));
+        pays.setPositionVille(3, new Position(positionX, 5));
+        pays.setPositionVille(4, new Position(positionX, 6));
 
         brutForce.recherche();
 
@@ -48,19 +69,20 @@ public class TestBrutForce {
     }
 
     @Test
-    public void test_ParcourOptimum_Pour_6villes() {
+    public void test_distanceLinaireSurY_ParcourOptimum_Pour_6villes_distance_linaire() {
 
         Pays pays = new Pays(6);
 
         brutForce = new BrutForce(pays);
 
-        pays.setPositionVille(0, new Position(2, 2));
-        pays.setPositionVille(1, new Position(2, 3));
-        pays.setPositionVille(2, new Position(2, 4));
-        pays.setPositionVille(3, new Position(2, 5));
-        pays.setPositionVille(4, new Position(2, 6));
-        pays.setPositionVille(5, new Position(2, 7));
+        int positionX = (int) Math.random() * 50;
 
+        pays.setPositionVille(0, new Position(positionX, 2));
+        pays.setPositionVille(1, new Position(positionX, 3));
+        pays.setPositionVille(2, new Position(positionX, 4));
+        pays.setPositionVille(3, new Position(positionX, 5));
+        pays.setPositionVille(4, new Position(positionX, 6));
+        pays.setPositionVille(5, new Position(positionX, 7));
 
         brutForce.recherche();
 
@@ -68,35 +90,54 @@ public class TestBrutForce {
 
     }
 
-    //TODO: 
     @Test
-    public void test_ParcourOptimum_Pour_6villes_desorde() {
-
+    public void test_distanceLinaireSurY_ParcourOptimum_Pour_6villes() {
 
         Pays pays = new Pays(6);
-        
+
         brutForce = new BrutForce(pays);
 
-        pays.setPositionVille(0, new Position(2, 2));
-        pays.setPositionVille(1, new Position(2, 3));
-        pays.setPositionVille(2, new Position(2, 4));
-        pays.setPositionVille(3, new Position(2, 5));
-        pays.setPositionVille(4, new Position(2, 6));
-        pays.setPositionVille(5, new Position(2, 7));
+        int positionX = (int) Math.random() * 50;
 
+        pays.setPositionVille(0, new Position(positionX, 2));
+        pays.setPositionVille(1, new Position(positionX, 3));
+        pays.setPositionVille(2, new Position(positionX, 4));
+        pays.setPositionVille(3, new Position(positionX, 5));
+        pays.setPositionVille(4, new Position(positionX, 6));
+        pays.setPositionVille(5, new Position(positionX, 7));
 
         brutForce.recherche();
 
-        double distanceMinimum= brutForce.getParcour().getDistance();
+        assertEquals("0123450", brutForce.getParcour().getVillesEmprunté());
 
+    }
 
-        pays.setPositionVille(0, new Position(2, 2));
-        pays.setPositionVille(4, new Position(2, 3));
-        pays.setPositionVille(5, new Position(2, 4));
-        pays.setPositionVille(3, new Position(2, 5));
-        pays.setPositionVille(1, new Position(2, 6));
-        pays.setPositionVille(2, new Position(2, 7));
+    @Test
+    public void test_distanceLinaireSurY_ParcourOptimum_Pour_6villes_desorde() {
 
+        Pays pays = new Pays(6);
+
+        int positionX = (int) Math.random() * 50;
+
+        brutForce = new BrutForce(pays);
+
+        pays.setPositionVille(0, new Position(positionX, 2));
+        pays.setPositionVille(1, new Position(positionX, 3));
+        pays.setPositionVille(2, new Position(positionX, 4));
+        pays.setPositionVille(3, new Position(positionX, 5));
+        pays.setPositionVille(4, new Position(positionX, 6));
+        pays.setPositionVille(5, new Position(positionX, 7));
+
+        brutForce.recherche();
+
+        double distanceMinimum = brutForce.getParcour().getDistance();
+
+        pays.setPositionVille(0, new Position(positionX, 2));
+        pays.setPositionVille(4, new Position(positionX, 3));
+        pays.setPositionVille(5, new Position(positionX, 4));
+        pays.setPositionVille(3, new Position(positionX, 5));
+        pays.setPositionVille(1, new Position(positionX, 6));
+        pays.setPositionVille(2, new Position(positionX, 7));
 
         brutForce.recherche();
 
@@ -104,17 +145,130 @@ public class TestBrutForce {
 
     }
 
+    // #endregion distance linaire sur Y
+
+    // #region distance linaire sur X
+
     @Test
-    public void test_ParcourOptimum_Pour_6villes_random() {
+    public void test_distanceLinaireSurX_distanceLinaireSurX_ParcourOptimum_Pour_4villes() {
+
+        Pays pays = new Pays(4);
+
+        int positionY = (int) Math.random() * 50;
+
+        brutForce = new BrutForce(pays);
+
+        pays.setPositionVille(0, new Position(2, positionY));
+        pays.setPositionVille(1, new Position(3, positionY));
+        pays.setPositionVille(2, new Position(4, positionY));
+        pays.setPositionVille(3, new Position(5, positionY));
+
+        brutForce.recherche();
+
+        assertEquals("01230", brutForce.getParcour().getVillesEmprunté());
+
+    }
+
+    @Test
+    public void test_distanceLinaireSurX_ParcourOptimum_Pour_5villes() {
+
+        Pays pays = new Pays(5);
+
+        brutForce = new BrutForce(pays);
+
+        int positionY = (int) Math.random() * 50;
+
+        pays.setPositionVille(0, new Position(2, positionY));
+        pays.setPositionVille(1, new Position(3, positionY));
+        pays.setPositionVille(2, new Position(4, positionY));
+        pays.setPositionVille(3, new Position(5, positionY));
+        pays.setPositionVille(4, new Position(6, positionY));
+
+        brutForce.recherche();
+
+        assertEquals("012340", brutForce.getParcour().getVillesEmprunté());
+
+    }
+
+    @Test
+    public void test_distanceLinaireSurX_ParcourOptimum_Pour_6villes_distance_linaire() {
 
         Pays pays = new Pays(6);
 
         brutForce = new BrutForce(pays);
 
+        int positionY = (int) Math.random() * 50;
+
+        pays.setPositionVille(0, new Position(2, positionY));
+        pays.setPositionVille(1, new Position(3, positionY));
+        pays.setPositionVille(2, new Position(4, positionY));
+        pays.setPositionVille(3, new Position(5, positionY));
+        pays.setPositionVille(4, new Position(6, positionY));
+        pays.setPositionVille(5, new Position(7, positionY));
 
         brutForce.recherche();
 
-        assertNotEquals("012345", brutForce.getParcour().getVillesEmprunté());
+        assertEquals("0123450", brutForce.getParcour().getVillesEmprunté());
 
     }
+
+    @Test
+    public void test_distanceLinaireSurX_ParcourOptimum_Pour_6villes() {
+
+        Pays pays = new Pays(6);
+
+        brutForce = new BrutForce(pays);
+
+        int positionY = (int) Math.random() * 50;
+
+        pays.setPositionVille(0, new Position(2, positionY));
+        pays.setPositionVille(1, new Position(3, positionY));
+        pays.setPositionVille(2, new Position(4, positionY));
+        pays.setPositionVille(3, new Position(5, positionY));
+        pays.setPositionVille(4, new Position(6, positionY));
+        pays.setPositionVille(5, new Position(7, positionY));
+
+        brutForce.recherche();
+
+        assertEquals("0123450", brutForce.getParcour().getVillesEmprunté());
+
+    }
+
+    @Test
+    public void test_distanceLinaireSurX_ParcourOptimum_Pour_6villes_desorde() {
+
+        Pays pays = new Pays(6);
+
+        int positionY = (int) Math.random() * 50;
+
+        brutForce = new BrutForce(pays);
+
+        pays.setPositionVille(0, new Position(2, positionY));
+        pays.setPositionVille(1, new Position(3, positionY));
+        pays.setPositionVille(2, new Position(4, positionY));
+        pays.setPositionVille(3, new Position(5, positionY));
+        pays.setPositionVille(4, new Position(6, positionY));
+        pays.setPositionVille(5, new Position(7, positionY));
+
+        brutForce.recherche();
+
+        double distanceMinimum = brutForce.getParcour().getDistance();
+
+        pays.setPositionVille(0, new Position(2, positionY));
+        pays.setPositionVille(4, new Position(3, positionY));
+        pays.setPositionVille(5, new Position(4, positionY));
+        pays.setPositionVille(3, new Position(5, positionY));
+        pays.setPositionVille(1, new Position(6, positionY));
+        pays.setPositionVille(2, new Position(7, positionY));
+
+        brutForce.recherche();
+
+        assertEquals(distanceMinimum, brutForce.getParcour().getDistance());
+
+    }
+
+    // #endregion distance linaire sur X
+
+
+
 }
