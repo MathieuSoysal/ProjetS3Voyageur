42f7b508b329defac21e5e57e6da64f12aea3472
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-10-06 00:06:27 +0200

    Nouvelle branche Comparateur de vitesse

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -1,10 +1,65 @@
 package projetS3Voyageur;
+
 import projetS3Voyageur.MathieuS.*;
+import projetS3Voyageur.MatthiasD.*;
 
 public class App {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello World ! c'est trop bien");
+        long startTime;
+        long endTime;
+
+        long timeMS;
+        long timeMD;
+
+        long tempsMoyenMS = 0;
+        long tempsMoyenMD = 0;
+
+        int pointMS = 0;
+        int pointMD = 0;
+
+        int fin = 15;
+        for (int i = 0; i < fin; i++) {
+            startTime = System.currentTimeMillis();
+            // #region MathieuS
+            Pays p = new Pays(10);
+            BrutForce b = new BrutForce(p);
+            b.recherche();
+            // #endregion MathieuS
+            endTime = System.currentTimeMillis();
+            timeMS = endTime - startTime;
+            tempsMoyenMS += timeMS / fin;
+
+            startTime = System.currentTimeMillis();
+            // #region MatthiasD
+            Graphe g = new Graphe(50, 10);
+            Resolveur r = new Resolveur(g, g.getOneSommet(0).getVille());
+            r.bruteForce();
+            // #endregion MatthiasD
+            endTime = System.currentTimeMillis();
+            timeMD = endTime - startTime;
+            tempsMoyenMD += timeMD / fin;
+
+            pointMD += (timeMD < timeMS) ? 1 : 0;
+            pointMS += (timeMS < timeMD) ? 1 : 0;
+
+            // System.out.println((((double)i)/((double)fin))*100);
+            int charge = (int) ((((double) i) / ((double) fin)) * 100);
+            System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n         [");
+            for (int j = 0; j < 100; j++) {
+                System.out.print((j <= charge) ? "#" : ".");
+            }
+            System.out.print("] \n \n \n \n");
+        }
+
+        System.out.println("\n   Branche Matthias Delon :\n" + "Points obtenue : " + pointMD
+                + "\n Temps moyen de recherche : " + tempsMoyenMD);
+        System.out.println("\n   Branche Mathieu Soysal :\n" + "Points obtenue : " + pointMS
+                + "\n Temps moyen de recherche : " + tempsMoyenMS);
+
+        System.out.println("\n L'algo le plus rapide a étais celui de la branche :"
+                + ((pointMS > pointMD) ? "Mathieu Soysal" : "Matthias Delon "));
+
     }
+
 }
---
+++ src/main/java/projetS3Voyageur/MatthiasD/Graphe.java
@@ -0,0 +1,84 @@
+package projetS3Voyageur.MatthiasD;
+
+import java.util.ArrayList;
+
+public class Graphe {
+
+    private ArrayList<Sommet> sommets;
+    private int taille;
+
+
+    //Constructeurs
+
+    public Graphe(int taille, int nbSommet) {
+        assert taille > 0: "la taille doit être strictement positive";
+
+        this.taille = taille;
+
+        this.sommets = new ArrayList<>();
+        genereSommet(nbSommet);
+    }
+
+
+    //Getters & Setters
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    public ArrayList<Sommet> getSommets() {
+        return sommets;
+    }
+
+    public void setSommets(ArrayList<Sommet> sommets) {
+        this.sommets = sommets;
+    }
+    public int getNbSommets(){
+        return this.sommets.size();
+    }
+
+
+    //Méthodes & Fonctions
+
+    /**
+     * @param s: Sommet
+     * @return {@code true} ssi le sommet {@code s} a été ajouté correctement.
+     */
+    public boolean ajouterSommet(Sommet s){
+        if(this.sommets.contains(s)){
+            return false;
+        }
+        return this.sommets.add(s);
+    }
+
+    /**
+     * @param s: Sommet
+     * @return {@code true} ssi le sommet {@code s} a été retiré correctement.
+     */
+    public boolean retirerSommet(Sommet s){
+        return this.sommets.remove(s);
+    }
+
+
+    public Sommet getOneSommet(int i){
+        return this.sommets.get(i);
+    }
+
+    public void genereSommet(int nbSommet){
+        for(int i = 0; i < nbSommet; i++){
+            this.sommets.add(new Sommet(this));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Graphe{" +
+                "sommets=" + sommets +
+                ", taille=" + taille +
+                '}';
+    }
+}
