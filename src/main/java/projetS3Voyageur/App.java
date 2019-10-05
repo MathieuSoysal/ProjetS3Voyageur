3b256fa03932676667658c87a0238c883c6e4f60
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-10-05 21:42:11 +0200

    Import MathieuS

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -1,8 +1,10 @@
 package projetS3Voyageur;
+import projetS3Voyageur.MathieuS.*;
 
 public class App {
 
     public static void main(String[] args) {
+        
         System.out.println("Hello World ! c'est trop bien");
     }
 }
