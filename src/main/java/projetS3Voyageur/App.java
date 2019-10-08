796dc26aadb1cfe2cd9747bd7acf3ed1feca8acc
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-10-08 17:23:38 +0200

    Ajout App

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -1,8 +1,14 @@
 package projetS3Voyageur;
 
+import projetS3Voyageur.ModesDeRecherches.BrutForce;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World ! c'est trop bien");
+
+        Pays france = new Pays(12);
+        Voyageur mrSmins = new Voyageur(france, 0);
+         System.out.println(mrSmins.getParcour(new BrutForce()));
+
     }
 }
