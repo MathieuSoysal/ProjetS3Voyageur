c7d2531fa33206ebd76cb20a9751d301564bb877
Nadir Zaïd <nadir.zaid@etu.umontpellier.fr>
2019-10-10 15:46:38 +0200

    CPU

--- src/main/java/projetS3Voyageur/ModesDeRecherches/CurrentCPU.java
+++ src/main/java/projetS3Voyageur/ModesDeRecherches/CurrentCPU.java
@@ -1,4 +1,44 @@
 package projetS3Voyageur.ModesDeRecherches;
 
-public class CurrentCPU {
+
+import java.lang.management.*;
+
+public class CurrentCPU implements Runnable {
+
+    public static void main(String[] args)
+            throws Exception {
+
+        long temps = System.nanoTime();
+        CurrentCPU test = new CurrentCPU();
+        synchronized (test) {
+            new Thread(test).start();
+            while (test.cpu == -1) {
+                test.wait();
+            }
+        }
+        System.out.println("Temps : " + (System.nanoTime() - temps) + " ms");
+        System.out.println("CPU : " + test.cpu + " ms");
+    }
+
+    private long cpu = -1;
+
+    public synchronized void run() {
+
+        try {
+
+            ThreadMXBean thread = ManagementFactory.getThreadMXBean();
+            long cpu = thread.getCurrentThreadCpuTime();
+            Thread.sleep(300);
+            long temps = System.nanoTime();
+            while (System.nanoTime() - temps < 700000000);
+            this.cpu = thread.getCurrentThreadCpuTime() - cpu;
+        }
+        catch (InterruptedException e) {}
+
+        finally {
+            notify();
+        }
+    }
 }
+
+
