6060b76d8db3813a4157665cf7941710c3957526
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-10-17 15:18:35 +0200

    Mise à jour : App.java

--- src/main/java/projetS3Voyageur/App.java
+++ src/main/java/projetS3Voyageur/App.java
@@ -1,37 +1,39 @@
 package projetS3Voyageur;
 
-import projetS3Voyageur.ModesDeRecherches.*;
-import projetS3Voyageur.StatsAlgos.Analyser;
-import projetS3Voyageur.StatsAlgos.Comparer;
-import projetS3Voyageur.StatsAlgos.GenererCSV;
+import projetS3Voyageur.ModesDeRecherches.BadTrackV2;
+import projetS3Voyageur.ModesDeRecherches.BrutForceV3_1;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        ModeRecherche[] listeAlgo = {new BrutForce(), new BrutForceV3(), new BrutForceV3_1(),new BrutForceV4(), new BadTrack(), new BadTrackV2(),new TrackProches()};
-        GenererCSV fichierCSV = new GenererCSV(listeAlgo, "Stats.csv");
-        fichierCSV.setTempsMax(8);
-        fichierCSV.setNbVillesMax(20);
-        fichierCSV.Genere();
+        // #region Generer un fichier CSV
+        // ModeRecherche[] listeAlgo = {new BrutForce(), new BrutForceV3(), new
+        // BrutForceV3_1(),new BrutForceV4(), new BadTrack(), new BadTrackV2(),new
+        // TrackProches()};
+        // GenererCSV fichierCSV = new GenererCSV(listeAlgo, "Stats.csv");
+        // fichierCSV.setTempsMax(8);
+        // fichierCSV.setNbVillesMax(20);
+        // fichierCSV.Genere();
 
+        // #region analyser approfondie d'un algo en particulier
         // Analyser analyse = new Analyser(new BrutForceV4());
         // analyse.setNbIteration(120);
-        //analyse.calculer();
+        // analyse.calculer();
         // analyse.afficher();
 
-        // byte nbVillesMax = (byte) 11;
-        // String[] attributs=new String[nbVillesMax+1];
-        // attributs[0] = "Algorithmes";
-        // for (byte i = 0; i < nbVillesMax; i++) {
-        //     attributs[i + 1] = String.valueOf(i + 3);
-        // }
-
-        // ModeRecherche[] listAlgo = { new BadTrackV2(),new TrackProches(), new PlusProcheV2(), new PlusProche() };
+        // #region comparer plusieurs algos :
+        // ModeRecherche[] listAlgo = {new BrutForceV4(), new BrutForceV4_1()};
         // Comparer compare = new Comparer(listAlgo);
         // compare.setNombreDeTest(1000);
         // compare.setNombreDeVilles(11);
         // compare.afficher();
+
+        Pays france = new Pays(15);
+        Voyageur mrSmins = new Voyageur(france, 0);
+
+        System.out.println(mrSmins.getParcours(new BadTrackV2()));
+
     }
 }
 // Pays france = new Pays(11);
