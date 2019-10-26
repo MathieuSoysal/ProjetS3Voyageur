392eac786112fcca866831c14fbb55e542c83694
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-10-26 18:37:50 +0200

    Ajout : commentaire explicatif : ModeRecherche

--- src/main/java/projetS3Voyageur/ModesDeRecherches/ModeRecherche.java
+++ src/main/java/projetS3Voyageur/ModesDeRecherches/ModeRecherche.java
@@ -1,9 +1,28 @@
 package projetS3Voyageur.ModesDeRecherches;
+
 import projetS3Voyageur.*;
 
-public interface ModeRecherche{
+public interface ModeRecherche {
 
-    public void recherche(Pays pays,int villeDepart);
+    /**
+     * Calcul en interne les chemins le plus court pour visité toutes les villes d'un pays
+     * 
+     * @param pays        Le pays concerné par la recherche
+     * @param villeDepart Le numéro de la ville de départ
+     */
+    public void recherche(Pays pays, int villeDepart);
+
+    /**
+     * Récupére le parcours le plus optimisé après la recherche() de celle-ci
+     * 
+     * @return {@code Parcours}
+     */
     public Parcours getParcour();
+
+    /**
+     * Récupère le nom de l'algortihme de recherche
+     * 
+     * @return {@code String}
+     */
     public String getNom();
 }
