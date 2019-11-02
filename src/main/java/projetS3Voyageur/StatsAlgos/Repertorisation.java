b3b511eb3a0daa52afcb899551a83d91255acfd6
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-11-02 23:50:26 +0100

    Ajout : Commentaire explicatif : Repertorisation

--- src/main/java/projetS3Voyageur/StatsAlgos/Repertorisation.java
+++ src/main/java/projetS3Voyageur/StatsAlgos/Repertorisation.java
@@ -7,12 +7,26 @@ import java.util.Map;
 
 public class Repertorisation {
 
+    /**
+     * Renvoie le chemin d'un répertoire en créant ou en choisissant les répertoires
+     * en fonction du nom de l'ordinateur et de la date actuelle dans le répertoire
+     * Statistique
+     * 
+     * @return {@code File} renvoie un répertoire en suivant le chemin :
+     *         Statistiques/{Nom Ordinateur}/{Date actuelle}/
+     */
     public static File getRepertoire() {
-        File f = new File("Statistiques/" + getNameOrdinateur()+"/"+LocalDate.now());
+        File f = new File("Statistiques/" + getNameOrdinateur() + "/" + LocalDate.now());
         f.mkdirs();
         return f;
     }
 
+    /**
+     * Essaye de récupérer (afin de le renvoyer) nom de l'ordinateur sinon renvois le
+     * nom de la session actuelle
+     * 
+     * @return {@code String} nom de l'ordinateur sinon nom de la session
+     */
     private static String getNameOrdinateur() {
         Map<String, String> env = System.getenv();
         if (env.containsKey("COMPUTERNAME"))
