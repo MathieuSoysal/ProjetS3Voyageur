64930e2c5cbb3267bc8f8af50f396cf26e506590
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-11-02 23:27:54 +0100

    Ajout : Commentaire explicatif : Texte

--- src/main/java/projetS3Voyageur/StatsAlgos/Texte.java
+++ src/main/java/projetS3Voyageur/StatsAlgos/Texte.java
@@ -4,12 +4,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Texte{
+public class Texte {
+
+    /**
+     * Ecrie dans un fichier texte (préalablement choisi) le texte mit en paramètre
+     * 
+     * @param fichier {@code File} Fichier qu'il faut crée/choisir
+     * @param texte   {@code String} Texte qu'il faut écrire
+     */
     public static void ecrire(File fichier, String texte) {
         try (FileWriter writer = new FileWriter(fichier)) {
             writer.write(texte);
             writer.close();
         } catch (IOException ioe) {
+            System.err.println("Erreur lors de la création/choix du fichier");
             ioe.printStackTrace();
         }
     }
