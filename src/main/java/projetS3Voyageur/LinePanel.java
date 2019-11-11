5a077e97a37fe67120f0f45e8cbc351d116805f5
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-11-11 02:43:37 +0100

    Première version : interface

--- src/main/java/projetS3Voyageur/LinePanel.java
+++ src/main/java/projetS3Voyageur/LinePanel.java
@@ -2,9 +2,12 @@ package projetS3Voyageur;
 
 import static java.lang.Math.random;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Collection;
@@ -12,18 +15,35 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import projetS3Voyageur.CompositionPays.Pays;
 
 /**
- * panneau de 300x200 pixels ou on dessine une ligne diagonale
+ * panneau de 300x200 pixels
  * 
  * @author jonathan
  */
 public class LinePanel extends JPanel implements MouseListener {
 
     private List<Point> points;
+    private List<Point> pointsOpti;
+
+    private int[] positionsX;
+    private int[] positionsY;
+
+    private JButton boutonReset = new JButton("Reinitialiser");
+    private JButton boutonCalculer = new JButton("Calculer");
+
+    private Pays graphe;
 
     public static void main(String[] args) {
 
@@ -36,6 +56,8 @@ public class LinePanel extends JPanel implements MouseListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // quand on clique sur la croix, ca quitte le programme
                                                               // (ce n'est pas le cas par defaut)
         frame.setVisible(true); // rend la fenetre visible (ce n'est pas le cas par defaut)
+        frame.setTitle("Voyageur de Commerce");
+
     }
 
     /**
@@ -44,46 +66,60 @@ public class LinePanel extends JPanel implements MouseListener {
     public LinePanel() {
 
         points = new LinkedList<>();
-        points.add(new Point((int) (random() * 300), (int) (random() * 200)));
-        points.add(new Point((int) (random() * 300), (int) (random() * 200)));
-        points.add(new Point((int) (random() * 300), (int) (random() * 200)));
-        points.add(new Point((int) (random() * 300), (int) (random() * 200)));
-        points.add(new Point((int) (random() * 300), (int) (random() * 200)));
 
-        setPreferredSize(new Dimension(300, 200)); // ajustement de la taille "preferee" du panneau
+        add(boutonReset);
+        add(boutonCalculer);
+        setPreferredSize(new Dimension(400, 400));
         addMouseListener(this);
+
+        boutonReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                points.clear();
+                
+            }
+          });
+
     }
 
     @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
 
-        graphics.create();
+        getPositionsAbs(points);
+        graphics.setColor(Color.gray);
+        graphics.drawPolyline(positionsX, positionsY, points.size() + 1);
 
-        Iterator<Point> pointIterator = points.iterator();
+        // getPositionsAbs(pointsOpti);
+        // graphics.setColor(Color.cyan);
+        // graphics.drawPolyline(positionsX, positionsY, points.size() + 1);
 
-        Point start;
+    }
 
-        int[] x = new int[points.size() + 1];
-        int[] y = new int[points.size() + 1];
-
-        for (int i = 0; pointIterator.hasNext(); i++) {
-            start = pointIterator.next();
-            x[i] = (int) start.getX();
-            y[i] = (int) start.getY();
+    private void getPositionsAbs(List<Point> points) {
+        int i = 0;
+        positionsY = new int[points.size() + 1];
+        positionsX = new int[points.size() + 1];
+        for (Point point : points) {
+            positionsY[i] = (int) point.getY();
+            positionsX[i++] = (int) point.getX();
         }
 
-        x[points.size()] = x[0];
-        y[points.size()] = y[0];
+        positionsY[points.size()] = positionsY[0];
+        positionsX[points.size()] = positionsX[0];
 
-        graphics.drawPolyline(x, y, points.size());
     }
 
     @Override
     public void mouseClicked(MouseEvent mouseEvent) {
-        Collections.shuffle(points);
-        repaint(); // on ne redessine que quand on a un nombre pair de points
+        points.add(getMousePosition());
+        repaint();
     }
+
+    // try {
+    //     Thread.sleep(3);
+    //   } catch (InterruptedException e) {
+    //     e.printStackTrace();
+    //   }
 
     @Override
     public void mouseEntered(MouseEvent mouseEvent) {
@@ -95,6 +131,8 @@ public class LinePanel extends JPanel implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
+        points.add(getMousePosition());
+        repaint();
     }
 
     @Override
