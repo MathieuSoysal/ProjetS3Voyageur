660c9ad9a496ea003a94b6be6cd650875e233e06
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-11-27 22:02:01 +0100

    Mise à jour des assert : TestGestionBD

--- src/test/java/projetS3Voyageur/bd/TestGestionBD.java
+++ src/test/java/projetS3Voyageur/bd/TestGestionBD.java
@@ -1,5 +1,7 @@
 package projetS3Voyageur.bd;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -17,20 +19,20 @@ public class TestGestionBD {
     public void test_requete() {
 
         InteractionBD.connexion();
-        assertEquals(InteractionBD.setRequete("SELECT * FROM Carte "), true);
+        assertTrue(InteractionBD.setRequete("SELECT * FROM Carte "));
     }
     @Test
     public void test_recuperation_() {
 
         InteractionBD.connexion();
-        assertEquals(InteractionBD.setRequete("SELECT nbVille FROM Carte WHERE idCarte = 1"), true);
+        assertTrue(InteractionBD.setRequete("SELECT nbVille FROM Carte WHERE idCarte = 1"));
     }
 
     @Test
     public void test_getNbVille_() {
 
         InteractionBD.connexion();
-        assertEquals(GestionBD.getNbVille("1"), 3);
+        assertEquals(3, GestionBD.getNbVille("1"));
     }
 /*58*//*ddf*/
    /* @Test
