30e425b0243f683b48ba5369311fde41f3e1079f
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-12-04 22:00:58 +0100

    Ajout test : TestKruskal : test_genererArbre_9noeuds

--- src/test/java/projetS3Voyageur/ModesDeRecherches/acm/TestKruskal.java
+++ src/test/java/projetS3Voyageur/ModesDeRecherches/acm/TestKruskal.java
@@ -3,7 +3,6 @@ package projetS3Voyageur.ModesDeRecherches.acm;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.awt.Point;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -11,6 +10,32 @@ import projetS3Voyageur.CompositionPays.Pays;
 
 public class TestKruskal {
     private Kruskal kruskal = new Kruskal();
+
+    @Test
+    public void test_genererArbre_9noeuds() {
+
+        // Trouver via calcul sur papier
+        final byte[][] listeAdjacenceNum = { { 3 }, { 8 }, {1, 5, 6 }, { 8, 9 }, { 3 }, { 3, 9, 7 }, { 6 }, { 2, 4 },
+                { 4, 6 } };
+        // TODO: réglé le problème avec le graphe planaire
+        final int[] listeAdjacence = valeurBinaire(listeAdjacenceNum, 9);
+        Pays pays = new Pays(9);
+
+        pays.setPositionVille(0, new Point(1, 1));
+        pays.setPositionVille(1, new Point(6, 4));
+        pays.setPositionVille(2, new Point(1, 3));
+        pays.setPositionVille(3, new Point(4, 2));
+        pays.setPositionVille(4, new Point(1, 4));
+        pays.setPositionVille(5, new Point(3, 3));
+        pays.setPositionVille(6, new Point(3, 5));
+        pays.setPositionVille(7, new Point(5, 3));
+        pays.setPositionVille(8, new Point(3, 2));
+
+        int i = 0;
+        for (int arete : kruskal.genereArbre(pays)) {
+            assertEquals(listeAdjacence[i++], arete);
+        }
+    }
 
     @Test
     public void test_genererArbre_8noeuds() {
@@ -82,7 +107,7 @@ public class TestKruskal {
     }
 
     @Test
-    public void test_genererArbre_6noeuds() {
+    public void test_genererArbre_6noeuds_part1() {
 
         // Trouver via calcul sur papier
         final byte[][] listeAdjacenceNum = { { 5, 6 }, { 5 }, { 4, 6 }, { 3 }, { 2, 1 }, { 1, 3 } };
@@ -103,6 +128,14 @@ public class TestKruskal {
         }
     }
 
+    /**
+     * Récupère une liste d'adjacence de numéro de noeud afin de renvoyer un tableau
+     * de int, (où chaque bite du int représente un noeud).
+     * 
+     * @param listeAdjacence {@code byte[][]} liste d'adjacence des numéros de noeud
+     * @param taille         {@code int} la taille du tableau de int à retourner
+     * @return {@code int[]} list d'adjacence sous forme binaire
+     */
     private int[] valeurBinaire(byte[][] listeAdjacence, int taille) {
         int[] resultat = new int[taille];
 
@@ -110,7 +143,7 @@ public class TestKruskal {
 
         for (byte[] noeudAdjacents : listeAdjacence) {
             for (byte noeud : noeudAdjacents) {
-                resultat[i] |= (noeud != 0) ? 1 << noeud - 1 : 0;
+                resultat[i] |= (noeud != 0) ? (1 << (noeud - 1)) : 0;
             }
             i++;
         }
