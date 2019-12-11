03987deb500d6994913852e73cab661e62811000
Florian-Ravet-Lecourt <47150560+Florian-Ravet-Lecourt@users.noreply.github.com>
2019-12-11 21:45:36 +0100

    Update VillesDuo.js

--- Site/js/VillesDuo.js
+++ Site/js/VillesDuo.js
@@ -4,6 +4,7 @@ while(!isINT(nbVilles) || nbVilles > 15 || nbVilles < 3){
     nbVilles = prompt('Entrez un nombre valide (entre 3 et 15) de villes:');
 }
 var totalVilles = nbVilles;
+let trajet = [];
 
 function setup() {
   createCanvas(400, 400);
@@ -12,15 +13,25 @@ function setup() {
   fill(0, 102);
   for (var i = 0; i < totalVilles; i++) {
     var v = createVector(random(width), random(height));
-    villes[i] = v;
+    villes[i] = (i, v);
   }
 }
 
 function draw() {
+  
+ // console.log("pi");
 
   fill(255,0,0);
   for (var i = 0; i < villes.length; i++) {
-    circle(villes[i].x, villes[i].y, 8);
+    ellipse(villes[i].x, villes[i].y, 8, 8);
+  }
+  
+  stroke(255, 0, 0);
+  strokeWeight(4);
+  
+  for (var i = 0; i < villes.length-1; i++) {
+    trajet.push(line(villes[i].x, villes[i].y, villes[i+1].x, villes[i+1].y));
+    //console.log(trajet);
   }
 }
 
@@ -30,7 +41,9 @@ function mouseClicked(){
     if(mouseX <= 400 && mouseX >= 0 && mouseY <= 400 && mouseY >= 0){
       //push a new circle to our array
       fill(255,0,0);
-      villes.push(circle(mouseX, mouseY, 8));
+      let nouvVilles = villes.slice(totalVilles++, 0, ellipse(mouseX, mouseY, 8, 8));
+      var v = createVector(mouseX, mouseY);
+      villes[villes.length] = (villes.length, v);
     }
   }
 }
