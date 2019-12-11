7ec7b0ffb171b730542c3f132a1b565e11a836ac
Florian-Ravet-Lecourt <47150560+Florian-Ravet-Lecourt@users.noreply.github.com>
2019-12-11 21:46:44 +0100

    Update VillesManuel.js

--- Site/js/VillesManuel.js
+++ Site/js/VillesManuel.js
@@ -1,5 +1,8 @@
 // noprotect
 let villes = [];
+let trajet = [];
+let totalVilles = villes.length-1;
+
 function setup() {
   createCanvas(400, 400);
   background(102);
@@ -12,6 +15,16 @@ function draw() {
     fill(255,0,0);
     circle(mouseX, mouseY, 8);
   }*/
+  if(villes.length > 2){
+  for (var i = 0; i < villes.length-1; i++) {
+    trajet.push(line(villes[i].x, villes[i].y, villes[i+1].x, villes[i+1].y));
+    //console.log(trajet);
+  }
+  }
+}
+
+function isINT(n){
+    return n == parseInt(n);
 }
 
 //add a circle whenever the mouse is clicked
@@ -21,9 +34,9 @@ function mouseClicked(){
     if(mouseX <= 400 && mouseX >= 0 && mouseY <= 400 && mouseY >= 0){
       //push a new circle to our array
       fill(255,0,0);
-      villes.push( circle(mouseX, mouseY, 8));
-      console.log(mouseX, mouseY);
-      console.log(villes.length);
+      let nouvVilles = villes.slice(totalVilles++, 0, ellipse(mouseX, mouseY, 8, 8));
+      var v = createVector(mouseX, mouseY);
+      villes[villes.length] = (villes.length, v);
     }
   }
 }
