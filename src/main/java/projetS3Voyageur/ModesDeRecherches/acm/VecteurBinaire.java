a8d71a19808faf8a43c52435f448df1d8b6b9c31
MathieuSoysal <Mathieu.soysal@outlook.fr>
2019-12-24 11:46:33 +0100

    Nettoyage : VecteurBinaire :  - Renommage  - Mise à jour commentaire

--- src/main/java/projetS3Voyageur/ModesDeRecherches/acm/VecteurBinaire.java
+++ src/main/java/projetS3Voyageur/ModesDeRecherches/acm/VecteurBinaire.java
@@ -2,95 +2,90 @@ package projetS3Voyageur.ModesDeRecherches.acm;
 
 public class VecteurBinaire {
     private int overflow;
-    private int valeurBinaire = 1; // TODO à voir si on le nomme pas emplacementBinaire ou un autre nom
-    private int listeNoireEmplacement;
+    private int valeurBinaire = 1;
+    private int listeNoireBits;
 
     private byte numeroNoeud;
 
     // #region Constructeurs
     /**
-     * Chacun des bits des vecteurs binaires représante un noeud, au sein de la
+     * Chacun des bits des vecteurs binaires représantent un noeud, au sein de la
      * class les vecteurs binaires sont stockés dans un {@code int}.
      * 
-     * @param valeurBinaire         Vecteur binaire dans {@code int} seul un de ces
-     *                              bits doit être à 1, celui-ci représante le noeud
-     *                              actuel.
-     * @param listeNoireEmplacement Vecteur de bits dans un {@code int} , chacun des
-     *                              des bits à 1 sont ignorés par la class
-     * @param taille                {@code byte} L'effectif des noeuds au sein du
-     *                              graphe.
+     * @param valeurBinaire  Vecteur binaire dans {@code int} seul un de ces bits
+     *                       doit être à 1, celui-ci représente le noeud actuel.
+     * @param listeNoireBits Vecteur de bits dans un {@code int} , chacun des bits à
+     *                       1 est ignoré par la class.
+     * @param taille         {@code byte} L'effectif des noeuds au sein du graphe.
      */
-    public VecteurBinaire(int valeurBinaire, int listeNoireEmplacement, byte taille) {
+    public VecteurBinaire(int valeurBinaire, int listeNoireBits, byte taille) {
         this.valeurBinaire = valeurBinaire;
-        initialise(listeNoireEmplacement, taille);
+        initialise(listeNoireBits, taille);
     }
 
     /**
-     * Chacun des bits des vecteurs binaires représante un noeud, au sein de la
+     * Chacun des bits des vecteurs binaires représantent un noeud, au sein de la
      * class les vecteurs binaires sont stockés dans un {@code int}.
      * 
-     * @param listeNoireEmplacement Vecteur de bits dans un {@code int} , chacun des
-     *                              des bits à 1 sont ignorés par la class
-     * @param taille                {@code byte} L'effectif des noeuds au sein du
-     *                              graphe.
+     * @param listeNoireBits Vecteur de bits dans un {@code int} , chacun des bits à
+     *                       1 est ignoré par la class.
+     * @param taille         {@code byte} L'effectif des noeuds au sein du graphe.
      */
-    private VecteurBinaire(int listeNoireEmplacement, byte taille) {
-        initialise(listeNoireEmplacement, taille);
+    private VecteurBinaire(int listeNoireBits, byte taille) {
+        initialise(listeNoireBits, taille);
     }
 
     /**
-     * Chacun des bits des vecteurs binaires représante un noeud, au sein de la
+     * Chacun des bits des vecteurs binaires représantent un noeud, au sein de la
      * class les vecteurs binaires sont stockés dans un {@code int}.
      * 
-     * @param listeNoireEmplacement Vecteur de bits dans un {@code int} , chacun des
-     *                              des bits à 1 sont ignorés par la class
-     * @param taille                {@code byte} L'effectif des noeuds au sein du
-     *                              graphe.
+     * @param listeNoireBits Vecteur de bits dans un {@code int} , chacun des bits à
+     *                       1 est ignoré par la class.
+     * @param taille         {@code byte} L'effectif des noeuds au sein du graphe.
      */
-    public static VecteurBinaire AvecTaille(int listeNoireEmplacement, byte taille) {
-        return new VecteurBinaire(listeNoireEmplacement, taille);
+    public static VecteurBinaire AvecTaille(int listeNoireBits, byte taille) {
+        return new VecteurBinaire(listeNoireBits, taille);
     }
 
     /**
-     * Chacun des bits des vecteurs binaires représante un noeud, au sein de la
+     * Chacun des bits des vecteurs binaires représantent un noeud, au sein de la
      * class les vecteurs binaires sont stockés dans un {@code int}.
      * 
-     * @param listeNoireEmplacement Vecteur de bits dans un {@code int} , chacun des
-     *                              des bits à 1 sont ignorés par la class
-     * @param OVERFLOW              Vecteur de bits dans un {@code int} où tous les
-     *                              bits représantant un noeud sont à 1 sauf le
-     *                              dernier bit.
+     * @param listeNoireBits Vecteur de bits dans un {@code int} , chacun des bits à
+     *                       1 est ignoré par la class.
+     * @param OVERFLOW       Vecteur de bits dans un {@code int} où tous les bits
+     *                       représentant un noeud sont à 1 sauf le dernier bit.
      */
-    private VecteurBinaire(int listeNoireEmplacement, final int OVERFLOW) {
-        initialise(listeNoireEmplacement, OVERFLOW);
+    private VecteurBinaire(int listeNoireBits, final int OVERFLOW) {
+        initialise(listeNoireBits, OVERFLOW);
     }
 
     /**
-     * Chacun des bits des vecteurs binaires représante un noeud, au sein de la
+     * Chacun des bits des vecteurs binaires représantent un noeud, au sein de la
      * class les vecteurs binaires sont stockés dans un {@code int}.
      * 
-     * @param listeNoireEmplacement Vecteur de bits dans un {@code int} , chacun des
-     *                              des bits à 1 sont ignorés par la class
-     * @param OVERFLOW              Vecteur de bits dans un {@code int} où tous les
-     *                              bits représantant un noeud sont à 1 sauf le
-     *                              dernier bit.
+     * @param listeNoireBits Vecteur de bits dans un {@code int} , chacun des bits à
+     *                       1 est ignoré par la class.
+     * @param OVERFLOW       Vecteur de bits dans un {@code int} où tous les bits
+     *                       représentant un noeud sont à 1 sauf le dernier bit.
      */
-    public static VecteurBinaire AvecOVERFLOW(int listeNoireEmplacement, final int OVERFLOW) {
-        return new VecteurBinaire(listeNoireEmplacement, OVERFLOW);
+    public static VecteurBinaire AvecOVERFLOW(int listeNoireBits, final int OVERFLOW) {
+        return new VecteurBinaire(listeNoireBits, OVERFLOW);
     }
     // #endregion Contructeurs
 
     // #region Méthodes public de la class
     /**
-     * @return {@code boolean} si le noeud actuel à un noeud suivant retourne
-     *         {@code true} sinon {@code false}.
+     * @return {@code boolean} si le bit (noeud) actuel n'est pas le dernier bit
+     *         (noeud) du vecteur de bits (graphe) retourne {@code true} sinon
+     *         {@code false}.
      */
     public boolean haseNext() {
         return valeurBinaire < overflow;
     }
 
     /**
-     * Passe au noeud suivant en fonction de la liste noire.
+     * Passe au bite (noeud) suivant en fonction de la liste noire.
      */
     public void next() {
         valeurBinaire <<= 1;
@@ -100,23 +95,23 @@ public class VecteurBinaire {
 
     // #region Outils interne à la class
     /**
-     * Verifie que la variable valeurBinaire n'est pas dans la liste noire si elle y
+     * Vérifie que la variable valeurBinaire n'est pas dans la liste noire si elle y
      * est elle change sa valeur pour qu'elle n'y soit plus.
      */
     private void verifieListeNoire() {
-        valeurBinaire += listeNoireEmplacement;
-        valeurBinaire ^= (valeurBinaire & listeNoireEmplacement);
+        valeurBinaire += listeNoireBits;
+        valeurBinaire ^= (valeurBinaire & listeNoireBits);
 
         numeroNoeud = (byte) Math.getExponent(valeurBinaire);
     }
 
-    private void initialise(int listeNoireEmplacement, byte taille) {
-        initialise(listeNoireEmplacement, (1 << (taille)) - 1);
+    private void initialise(int listeNoireBits, byte taille) {
+        initialise(listeNoireBits, (1 << (taille)) - 1);
     }
 
-    private void initialise(int listeNoireEmplacement, final int OVERFLOW) {
+    private void initialise(int listeNoireBits, final int OVERFLOW) {
         this.overflow = OVERFLOW;
-        this.listeNoireEmplacement = listeNoireEmplacement;
+        this.listeNoireBits = listeNoireBits;
         verifieListeNoire();
     }
     // #endregion Outils interne à la class
@@ -125,7 +120,7 @@ public class VecteurBinaire {
     /**
      * 
      * @return Chaque bit du {@code int} retourné représente un noeud, seul le bit
-     *         représantant le noeud actuel est à 1.
+     *         représentant le noeud actuel est à 1.
      */
     public int getValeurBinaire() {
         return valeurBinaire;
